f90b7356676494f16bc175bfc5b66ef4248c7372
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-05-26 17:21:27 -0300

    feat: post

--- src/main/java/com/example/springboot/Controllers/ProductController.java
+++ src/main/java/com/example/springboot/Controllers/ProductController.java
@@ -1,8 +1,18 @@
 package com.example.springboot.Controllers;
 
+import com.example.springboot.dtos.ProductRecordDTO;
+import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 public class ProductController {
@@ -10,5 +20,11 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDTO, productModel);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
 }
