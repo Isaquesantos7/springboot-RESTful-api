67fee5cb51e3ce275d060b368ac1dba2a5f7dc5d
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-05-27 20:33:24 -0300

    feat: getAll

--- src/main/java/com/example/springboot/Controllers/ProductController.java
+++ src/main/java/com/example/springboot/Controllers/ProductController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
+import java.util.List;
+
 
 @RestController
 public class ProductController {
@@ -20,11 +22,17 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    @PostMapping("/products")
+    @PostMapping("/api/products")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDTO) {
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDTO, productModel);
         return  ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/api/products")
+    public ResponseEntity<List<ProductModel>> findAllProducts() {
+
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
 
 }
