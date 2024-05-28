f279e669e528fea543d009530d12856011bf5062
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-05-27 21:16:49 -0300

    feat: delete product

--- src/main/java/com/example/springboot/Controllers/ProductController.java
+++ src/main/java/com/example/springboot/Controllers/ProductController.java
@@ -59,4 +59,17 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
 
     }
+
+    @DeleteMapping("/api/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> product0 = productRepository.findById(id);
+
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found!");
+        }
+
+        var productModel = product0.get();
+        productRepository.delete(productModel);
+        return ResponseEntity.status(HttpStatus.OK).body("Product was deleted with success!");
+    }
 }
